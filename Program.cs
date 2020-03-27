dca434ee48d5db4f18f8c9f5c7f1fd07f6df0431
faha-berdiev <madysoftware@gmail.com>
2020-03-27 14:28:34 +0300

    Проверка суммы покупки

--- Program.cs
+++ Program.cs
@@ -7,7 +7,14 @@ namespace Day_4
         static void Main(string[] args)
         {
             //Task 1...
-            
+            Console.WriteLine("Task 1...");
+            Console.Write("Введите сумму покупки: ");
+            decimal _pay = Convert.ToDecimal(Console.ReadLine()); //Сумма покупки
+            decimal _discount = 0m; //Скидка
+            if(_pay > 500)
+               _discount = 3m;
+            else if (_pay > 1000)
+                _discount = 5m;
             ////////////////////////////////////////////////
         }
     }
