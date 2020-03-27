85af3ba84b98ca48dcce2e931a22d13296bc2417
faha-berdiev <madysoftware@gmail.com>
2020-03-27 14:20:36 +0300

    allif hw 26-03-2020

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1...
+            
+            ////////////////////////////////////////////////
+        }
+    }
+}
